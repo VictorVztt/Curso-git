f309cb0a35cedd7fb473c41d5a601573185ecf9c
Victor Vazatta <victor.vazatta@gmail.com>
2021-11-10 17:46:43 -0300

    Testando segundo

--- ProjetoTesteGit/Program.cs
+++ ProjetoTesteGit/Program.cs
@@ -7,6 +7,8 @@ namespace ProjetoTesteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Teste para mod");
+            
         }
     }
 }
